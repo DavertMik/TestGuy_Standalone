8e3144bfe010d0f8a6fae2a907af62cc6825f56c
unknown <D:\Bm\M@il>
2011-08-18 16:27:09 +0300

    added test

--- tests/testguy/self/BuildSpec.php
+++
@@ -1,7 +0,0 @@
-<?php
-$I = new TestGuy($scenario);
-$I->wantToTest('build command');
-$I->execute('php testguy.php build --silent');
-$I->seeInOutput('generated sucessfully');
-$I->findFile('TestGuy.php','tests');
-$I->seeInFile('@method void seeInFile($text)');
